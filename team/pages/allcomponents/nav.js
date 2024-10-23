6afedc7d10a5b55ac66286d2919e44bf4e79a472
kimlengkit1 <kkit@umass.edu>
2024-10-22 20:41:56 -0400

    link nav to pages

--- team/pages/allcomponents/nav.js
+++ team/pages/allcomponents/nav.js
@@ -2,6 +2,6 @@ const nav = document.getElementById('navbar');
 
 console.log(nav);
 
-nav.innerHTML += "<li><a href='/home/index.html'>Event tbd</a></li>";
-nav.innerHTML += "<li><a href='/about/index.html'>About Us</a></li>";
-nav.innerHTML += "<li><a href='/event/index.html'>Events</a></li>";
+nav.innerHTML += "<li><a href='../home/index.html'>Event tbd</a></li>";
+nav.innerHTML += "<li><a href='../about/index.html'>About Us</a></li>";
+nav.innerHTML += "<li><a href='../event/index.html'>Events</a></li>";
